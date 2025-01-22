44822c36a250d9ac17d1e8603de072642507db42
JoseRobertoGDeveloper <joserobertoq@outlook.com>
2025-01-22 08:46:05 -0600

    Agregar ejemplos de entrada y salida en Python

---
+++ 01_basic/05_input.py
@@ -0,0 +1,13 @@
+# nombre = input("Hola, cómo te llamas?") 
+
+# print(f"Hola {nombre}, cómo estás?")
+
+# age = input("¿Cuántos años tienes?")
+# age = int(age)
+
+# print(f"Hola {nombre}, tengo {age + 20} años")
+
+# Print para obener varios valores de entrada
+country, city = input("¿En qué pais y ciudad vives? \n").split()
+
+print(f"Vives en {country} y {city}")  
